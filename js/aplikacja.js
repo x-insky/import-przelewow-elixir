d466e9f0866e16751d2c6a5f255bdb10cc82eebe
x-insky <45213774+x-insky@users.noreply.github.com>
2021-12-29 23:20:21 +0100

    nested navigaion works fine
    * a nested navigation for generating a transfer has been prepared
       - also introductory five stages, so many and navigation elements
       - empty containers for elements, with text identifiers only
    * changed the logic of previous matches for searching for elements of  transfer
       - new classes have been added for the distinguishing features of navigation elements and transfer stages
    * logic of transitions for stages of transfers built on the basis of navigation between the main sections of the website
       - scrolling between transfer steps works properly!

--- js/aplikacja.js
+++ js/aplikacja.js
@@ -1,18 +1,15 @@
-var odnosnikiMenuNawigacyjnego = document.querySelectorAll('nav a'),    // każdy <a> w <nav>
-    podstronyGlownychDzialow = document.querySelectorAll('main .kontener-pozaekranowy section'),
-    kontenerPodstronGlownychDzialow = document.querySelector('main .kontener-pozaekranowy > article'); 
-
-// console.log(odnosnikiMenuNawigacyjnego.length, ': ', odnosnikiMenuNawigacyjnego);
- console.log(podstronyGlownychDzialow.length, ': ', podstronyGlownychDzialow);
-
-podstronyGlownychDzialow
+var odnosnikiMenuNawigacyjnego = document.querySelectorAll('header nav a'),    // każdy <a> w <nav>
+    kontenerPodstronGlownychDzialow = document.querySelector('.glowne-dzialy > article'),    
+    podstronyGlownychDzialow = document.querySelectorAll('.glowne-dzialy > article > section'),
+    kontenerEtapowPrzelewow = document.querySelector('.etapy-przelewow > article'),
+    podstronyEtapowPrzelewow = document.querySelectorAll('.etapy-przelewow > article > section'),
+    odnosnikiEtapowPrzelewow = document.querySelectorAll('.nawigacja-etapow a'); 
 
 odnosnikiMenuNawigacyjnego.forEach( function(odnosnik, idx ) {
     console.log( odnosnik );
 
-    odnosnik.addEventListener('click', function ( evt ) {
+    odnosnik.addEventListener('click', function ( evt ) {   // reakcja na nawigację DZIAŁÓW
         evt.preventDefault();   // wyłączenie domyślnego zachowania - brak przejścia do elementu docelowego 
-
 
         var nrDzialu = parseInt( evt.target.getAttribute('data-nr') - 1 ) || 0; // odczytanie atrybutu wyzwalacza (-1 dla liczenia od zera), asekuracja zerem
         nrDzialu = ( nrDzialu === idx ) ? idx : 0;  // werfikacja braku grzebania w strukturze
@@ -25,6 +22,28 @@ odnosnikiMenuNawigacyjnego.forEach( function(odnosnik, idx ) {
         /* var doTranslacji = ( idx * ( 100 / podstronyGlownychDzialow.length) ).toString() + '%';
         console.log( doTranslacji );
         kontenerPodstronGlownychDzialow.style.transform = 'translateX(' + doTranslacji + ')'; */
+
+    }, false);
+});
+
+console.log('kontenerEtapowPrzelewow', kontenerEtapowPrzelewow.length, ': ', kontenerEtapowPrzelewow);
+console.log('podstronyEtapowPrzelewow', podstronyEtapowPrzelewow.length, ': ', podstronyEtapowPrzelewow);
+console.log('odnosnikiEtapowPrzelewow', odnosnikiEtapowPrzelewow.length, ': ', odnosnikiEtapowPrzelewow);
+
+
+odnosnikiEtapowPrzelewow.forEach( function(odnosnik, idx ) {    // reakcja na nawigację ETAPÓW
+    console.log( odnosnik );
+
+    odnosnik.addEventListener('click', function ( evt ) {
+        evt.preventDefault();   // wyłączenie domyślnego zachowania - brak przejścia do elementu docelowego 
+
+        var nrEtapu = parseInt( evt.target.getAttribute('data-nr') - 1 ) || 0; // odczytanie atrybutu wyzwalacza (-1 dla liczenia od zera), asekuracja zerem
+        nrEtapu = ( nrEtapu === idx ) ? idx : 0;  // werfikacja braku grzebania w strukturze
+        console.log( evt.target, evt.target.getAttribute('data-nr'), nrEtapu, idx );
+
+        var stylLeft = ( idx * (-1 * 100 / podstronyEtapowPrzelewow.length) ).toString() + '%'; // odjęcie widoku o skok o widoczny elemnet (co X%), zależnie od ilości elementów
+        console.log( idx, stylLeft );
+        kontenerEtapowPrzelewow.style.left = stylLeft;
 
     }, false);
 });
