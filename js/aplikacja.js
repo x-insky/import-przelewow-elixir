1175345fff63b6b1c5328329dcbd1771033c402d
x-insky <45213774+x-insky@users.noreply.github.com>
2021-12-31 17:09:57 +0100

    first refactoring - click event handling with content scrolling
    * avoiding repeatability
       - the same code previously written twice, only with reference to other variables
      - shorter & cleaner code
    * defined  a function with the necessary list of attributes to handle the event and reference changes to the UI target
       - so far without complex verification (e.g. in terms of security or correctness)
    * added separtors for code beautify inside comments

--- js/aplikacja.js
+++ js/aplikacja.js
@@ -1,49 +1,55 @@
 var odnosnikiMenuNawigacyjnego = document.querySelectorAll('header nav a'),    // każdy <a> w <nav>
     kontenerPodstronGlownychDzialow = document.querySelector('.glowne-dzialy > article'),    
     podstronyGlownychDzialow = document.querySelectorAll('.glowne-dzialy > article > section'),
+    odnosnikiEtapowPrzelewow = document.querySelectorAll('.nawigacja-etapow a'),
     kontenerEtapowPrzelewow = document.querySelector('.etapy-przelewow > article'),
-    podstronyEtapowPrzelewow = document.querySelectorAll('.etapy-przelewow > article > section'),
-    odnosnikiEtapowPrzelewow = document.querySelectorAll('.nawigacja-etapow a'); 
+    podstronyEtapowPrzelewow = document.querySelectorAll('.etapy-przelewow > article > section');
 
-odnosnikiMenuNawigacyjnego.forEach( function(odnosnik, idx ) {
-    console.log( odnosnik );
 
-    odnosnik.addEventListener('click', function ( evt ) {   // reakcja na nawigację DZIAŁÓW
-        evt.preventDefault();   // wyłączenie domyślnego zachowania - brak przejścia do elementu docelowego 
+        /*  ************************************************
+                        F   U   N   K   C   J   E           
+            ************************************************
+        */
 
-        var nrDzialu = parseInt( evt.target.getAttribute('data-nr') - 1 ) || 0; // odczytanie atrybutu wyzwalacza (-1 dla liczenia od zera), asekuracja zerem
-        nrDzialu = ( nrDzialu === idx ) ? idx : 0;  // werfikacja braku grzebania w strukturze
-        console.log( evt.target, evt.target.getAttribute('data-nr'), nrDzialu, idx );
 
-        var stylLeft = ( idx * (-1 * 100 / podstronyGlownychDzialow.length) ).toString() + '%'; // odjęcie widoku o skok o widoczny elemnet (co X%), zależnie od ilości elementów
-        console.log( stylLeft );
-        kontenerPodstronGlownychDzialow.style.left = stylLeft;
+function powiazZdarzenieKliknieciaWNawigacjiZPrzewinieciemKontenera ( listaOdnosnikowPrzewijajacych, kontenerElementowSterowanych, ileElementowSterowanych ) {
 
-        /* var doTranslacji = ( idx * ( 100 / podstronyGlownychDzialow.length) ).toString() + '%';
-        console.log( doTranslacji );
-        kontenerPodstronGlownychDzialow.style.transform = 'translateX(' + doTranslacji + ')'; */
+    ileElementowSterowanych = ileElementowSterowanych || 1;
 
-    }, false);
-});
+    listaOdnosnikowPrzewijajacych.forEach( function( odnosnik, idx ) {    
+        console.log( odnosnik );
+    
+        odnosnik.addEventListener('click', function ( evt ) {
+            evt.preventDefault();   // wyłączenie domyślnego zachowania - brak przejścia do elementu docelowego 
+    
+            var nrEtapu = parseInt( evt.target.getAttribute('data-nr') - 1 ) || 0; // odczytanie atrybutu wyzwalacza (-1 dla liczenia od zera), asekuracja zerem
+            nrEtapu = ( nrEtapu === idx ) ? idx : 0;  // werfikacja braku grzebania w strukturze
+            console.log( evt.target, evt.target.getAttribute('data-nr'), nrEtapu, idx );
+    
+            var stylLeft = ( idx * ( -1 * 100 / ileElementowSterowanych ) ).toString() + '%'; // odjęcie widoku o skok o widoczny element (co X%), zależnie od ilości elementów
+            console.log( idx, stylLeft );
+            kontenerElementowSterowanych.style.left = stylLeft;
+
+                // prawdopodobnie lepszy sposób na przewijanie elementów docelowych (działów/etapów/obrazków/...)
+            /* var doTranslacji = ( idx * ( 100 / podstronyGlownychDzialow.length) ).toString() + '%';
+            console.log( doTranslacji );
+            kontenerPodstronGlownychDzialow.style.transform = 'translateX(' + doTranslacji + ')'; */
+    
+        }, false);
+    });
+}   // powiazZdarzenieKliknieciaWNawigacjiZPrzewinieciemKontenera-END
+
+
+        /*  *****************************************************
+                U   R   U   C   H   O   M   I   E   N   I   E           
+            *****************************************************
+        */
+
+
+powiazZdarzenieKliknieciaWNawigacjiZPrzewinieciemKontenera( odnosnikiMenuNawigacyjnego, kontenerPodstronGlownychDzialow, podstronyGlownychDzialow.length );   // reakcja na nawigację DZIAŁÓW WITRYNY 
 
 console.log('kontenerEtapowPrzelewow', kontenerEtapowPrzelewow.length, ': ', kontenerEtapowPrzelewow);
 console.log('podstronyEtapowPrzelewow', podstronyEtapowPrzelewow.length, ': ', podstronyEtapowPrzelewow);
 console.log('odnosnikiEtapowPrzelewow', odnosnikiEtapowPrzelewow.length, ': ', odnosnikiEtapowPrzelewow);
 
-
-odnosnikiEtapowPrzelewow.forEach( function(odnosnik, idx ) {    // reakcja na nawigację ETAPÓW
-    console.log( odnosnik );
-
-    odnosnik.addEventListener('click', function ( evt ) {
-        evt.preventDefault();   // wyłączenie domyślnego zachowania - brak przejścia do elementu docelowego 
-
-        var nrEtapu = parseInt( evt.target.getAttribute('data-nr') - 1 ) || 0; // odczytanie atrybutu wyzwalacza (-1 dla liczenia od zera), asekuracja zerem
-        nrEtapu = ( nrEtapu === idx ) ? idx : 0;  // werfikacja braku grzebania w strukturze
-        console.log( evt.target, evt.target.getAttribute('data-nr'), nrEtapu, idx );
-
-        var stylLeft = ( idx * (-1 * 100 / podstronyEtapowPrzelewow.length) ).toString() + '%'; // odjęcie widoku o skok o widoczny elemnet (co X%), zależnie od ilości elementów
-        console.log( idx, stylLeft );
-        kontenerEtapowPrzelewow.style.left = stylLeft;
-
-    }, false);
-});
+powiazZdarzenieKliknieciaWNawigacjiZPrzewinieciemKontenera( odnosnikiEtapowPrzelewow, kontenerEtapowPrzelewow, podstronyEtapowPrzelewow.length );    // reakcja na nawigację ETAPÓW PRZELEWÓW (zagnieżdżona podkategoria)
